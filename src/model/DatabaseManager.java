a58ea924dd341aef74b84a8e7f28725e04778a25
faithfulPurpleCap <dtomid777@yahoo.com>
2026-03-17 11:31:31 -0400

    BufferedReader and BufferedWriter, filepaths
    Added BufferedReader, BufferedWriter and file paths to DatabaseManager for CSV file reading and writing

--- src/model/DatabaseManager.java
+++ src/model/DatabaseManager.java
@@ -3,14 +3,52 @@ import model.User.*;
 import model.Equipment.*;
 import model.Payment.*;
 import model.Reservation.*;
-import java.io.File;
+import java.io.*;
 import java.util.ArrayList;
 
 public class DatabaseManager {
 
 	private static DatabaseManager instance = null;
 	
+	//CSV file paths
+	private String userFilePath = "/EECS3311-YorkU-Lab-Equipment-Reservation-Platform-Group19/src/User.csv";
+	private String equipmentFilePath = "/EECS3311-YorkU-Lab-Equipment-Reservation-Platform-Group19/src/Equipment.csv";
+	private String paymentFilePath = "/EECS3311-YorkU-Lab-Equipment-Reservation-Platform-Group19/src/Payment.csv";
+	private String reservationFilePath = "/EECS3311-YorkU-Lab-Equipment-Reservation-Platform-Group19/src/Reservation.csv";
+	
+	// line for reading data
+	private String line = "";
+	
+	// initialize BufferedReader instances
+	private BufferedReader userFileReader = null;
+	private BufferedReader equipmentFileReader = null;
+	private BufferedReader paymentFileReader = null;
+	private BufferedReader reservationFileReader = null;
+	
+	// initialize BufferedWriter instances
+	private BufferedWriter userFileWriter = null;
+	private BufferedWriter equipmentFileWriter = null;
+	private BufferedWriter paymentFileWriter = null;
+	private BufferedWriter reservationFileWriter = null;
+	
 	private DatabaseManager() {
+		try {
+			//Define BufferedReaders
+			userFileReader = new BufferedReader(new FileReader(userFilePath));
+			equipmentFileReader = new BufferedReader(new FileReader(equipmentFilePath));
+			paymentFileReader = new BufferedReader(new FileReader(paymentFilePath));
+			reservationFileReader = new BufferedReader(new FileReader(reservationFilePath));
+			
+			//Define BufferedWriters
+			userFileWriter = new BufferedWriter(new FileWriter(userFilePath));
+			equipmentFileWriter = new BufferedWriter(new FileWriter(equipmentFilePath));
+			paymentFileWriter = new BufferedWriter(new FileWriter(paymentFilePath));
+			reservationFileWriter = new BufferedWriter(new FileWriter(reservationFilePath));
+		}catch(Exception e){
+			System.out.println("ERROR instansiating File Readers/Writers");
+		}finally {
+			
+		}
 		
 	}
 	
@@ -24,6 +62,7 @@ public class DatabaseManager {
 	}
 	
 	public ArrayList<User> loadUsers(){
+		ArrayList<User> users = new ArrayList<User>();
 		return null;
 	}
 	
