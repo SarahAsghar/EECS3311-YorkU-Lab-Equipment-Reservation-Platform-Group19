319147474c2a717e1777fe0ac1c3639e6bb6bb5d
Sarah Asghar <sarahasghar126@gmail.com>
2026-03-19 03:41:01 -0400

    fixed initialized

--- src/model/Initialize.java
+++ src/model/Initialize.java
@@ -12,8 +12,7 @@ public class Initialize {
 	public static void main(String[] args) {
 		System.out.print("HI I AM WORKING :)");
 		LoginView start = LoginView.getInstance();
-		start.setLoginViewVisibility(false);
-		new AdminView(UserType.HEADLABCOORDINATOR, null, null);
+		
 		
 	}
 
