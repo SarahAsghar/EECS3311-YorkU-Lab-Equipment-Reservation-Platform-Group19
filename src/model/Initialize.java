a3b02b562e266d304161bf203561733c14e6d203
Sarah Asghar <sarahasghar126@gmail.com>
2026-03-21 20:19:26 -0400

    Fixed initialised

--- src/model/Initialize.java
+++ src/model/Initialize.java
@@ -24,14 +24,8 @@ public class Initialize {
 		db.saveUsers(users);
 		
 		UserController.setLoggedInUser(u);
-//		ReservationView v = new ReservationView(new ArrayList<Equipment>(), new AdminView(null, null, users, null));
 		LoginView start = LoginView.getInstance();
-		start.setLoginViewVisibility(false);
-		ReservationView vs = new ReservationView(new ArrayList<Equipment>(), new AdminView(UserType.STUDENT, new ArrayList<Reservation> (),  users, new ArrayList<Equipment> ()));
-
-//		AdminView v = new AdminView(UserType.LABMANAGER, new ArrayList<Reservation>() , new ArrayList<User>());
-		
-		
+			
 	}
 
 }
