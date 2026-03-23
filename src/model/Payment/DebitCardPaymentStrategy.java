6b849e91e749c9b533567d7f17d4ae5f8e9c6e6a
Sadia Jahan <sadiajahansh234@gmail.com>
2026-03-23 01:21:16 -0400

    Update DebitCardPaymentStrategy.java

--- src/model/Payment/DebitCardPaymentStrategy.java
+++ src/model/Payment/DebitCardPaymentStrategy.java
@@ -1,5 +1,18 @@
 package model.Payment;
 
-public class DebitCardPaymentStrategy {
+public class DebitCardPaymentStrategy implements PaymentStrategy {
 
+    @Override
+    public boolean processPayment(double amount) {
+        if (amount <= 0) {
+            return false;
+        }
+        System.out.println("debit card payment processing = " + amount);
+        return true;
+    }
+
+    @Override
+    public String getPaymentType() {
+        return "Debit Card";
+    }
 }
