e364c1b278b1bbde718ae5efb4d190707b6da569
Sadia Jahan <sadiajahansh234@gmail.com>
2026-03-23 01:21:35 -0400

    Update InstitutionalAccountPaymentStrategy.java

--- src/model/Payment/InstitutionalAccountPaymentStrategy.java
+++ src/model/Payment/InstitutionalAccountPaymentStrategy.java
@@ -1,5 +1,18 @@
 package model.Payment;
 
-public class InstitutionalAccountPaymentStrategy {
+public class InstitutionalAccountPaymentStrategy implements PaymentStrategy {
 
+    @Override
+    public boolean processPayment(double amount) {
+        if (amount <= 0) {
+            return false;
+        }
+        System.out.println("institutional payment processing" + amount);
+        return true;
+    }
+
+    @Override
+    public String getPaymentType() {
+        return "Institutional Account";
+    }
 }
